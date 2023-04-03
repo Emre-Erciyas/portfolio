2afc868ab1394413351cb19762c9a0035e22f63f
Emre <emre140498@hotmail.com>
2023-04-03 14:59:16 +0300

    projects responsive

--- components/project.tsx
+++ components/project.tsx
@@ -1,27 +1,44 @@
 import Image, { StaticImageData } from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { IconType } from 'react-icons/lib'
+import {HiOutlineExternalLink} from 'react-icons/hi'
+import { EB_Garamond } from 'next/font/google'
+
+const garamond = EB_Garamond({ weight: '400', subsets: ['latin'] })
+const garamondBold = EB_Garamond({ weight: '700', subsets: ['latin'] })
 
 interface Props {
   staticIMG: StaticImageData,
   name: String,
   description: String,
-  stack: string[]
+  stack: IconType[],
+  isLive: boolean,
+  source: string,
+  live: string
 }
 
-export default function Project(props: Props) {
+export default function Project(props: Props){
   return (
-    <div className='w-[90%] lg:w-3/4 bg-[#e5e5f3] my-3 lg:my-5 rounded-lg shadow-lg  lg:ml-[10%] flex flex-col lg:flex-row'>
-
-        <Image 
-          className='rounded-lg my-3 ml-3'
-          src={props.staticIMG} 
-          alt = 'Project Image'
-          priority
-        />
-  
-      <div className='flex flex-col w-1/2 h-full items-center mx-3'>
-        <h1 className='mt-3 text-[#2B2B33] text-xl'>{props.name}</h1>
-        <p className='mt-3 mx-6 text-[#68687A] text-lg tracking-wide italic'>{props.description}</p>
+    <div className='relative w-[95%] md:h-[400px] xl:h-[440px] xl:w-3/4 bg-[#e5e5f3] my-3 md:my-5 rounded-lg shadow-lg md:ml-5 xl:ml-[10%] flex flex-col items-center md:flex-row'>
+      <Image 
+        className='rounded-lg md:m-5 mt-5  md:mr-0 w-[95%] md:max-w-[min(480px,_60%)] lg:max-w-[min(560px,_60%)] xl:max-w-[min(600px,_60%)]'
+        src={props.staticIMG} 
+        alt = 'Project Image'
+        priority
+      />
+      <div className='relative flex flex-col md:w-1/2 items-center mx-3 md:h-[400px] xl:h-[440px]'>
+        <h1 className='mt-5 text-[#2B2B33] text-xl'>{props.name}</h1>
+        <p className='mt-3 mx-6 md:mx-3 xl:mx-6 text-[#68687A] 3xl:text-lg tracking-wide italic text-sm xl:text-base'>{props.description}</p>
+        <div className='w-full mt-6 flex justify-center mb-24'>
+          {props.stack.map((Element: IconType) =>{
+            return <Element className=' text-3xl text-[#2B2B33] mx-5'/>
+          })}
+        </div>
+        <div className={`w-full flex absolute bottom-4 md:bottom-8 justify-around`}>
+          <Link href = {props.source} className='flex items-center xl:text-base text-sm bg-[#B1B1BD] py-2 px-4 rounded tracking-wide shadow-sm'>Source Code {<HiOutlineExternalLink className='ml-1' />}</Link>
+          <Link href = {props.live} className={`flex items-center xl:text-base text-sm bg-[#B1B1BD] py-2 px-4 rounded tracking-wide shadow-sm ${!props.isLive && 'hidden'}`}>Demo {<HiOutlineExternalLink className='ml-1' />}</Link>
+        </div>
       </div>
 
     </div>
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,29 +1,36 @@
 'use client'
 import React from 'react'
-import { EB_Garamond } from 'next/font/google'
 import Project from './project'
 import Crush from '@/public/Crush.svg'
+import {SiNextdotjs, SiFirebase, SiTailwindcss, SiTypescript} from 'react-icons/si'
+
+
 //import Portfolio from '@/public/Portfolio.svg'
 
 
-const garamond = EB_Garamond({ weight: '400', subsets: ['latin'] })
 export default function Projects() {
   
   return (
-    <div className={`w-full bg-gradient-to-br to-[#B1B1BD] from-[#e1e1f0] m-0 ${garamond.className}`}>
+    <div className={`w-full bg-gradient-to-br to-[#B1B1BD] from-[#e1e1f0] m-0`}>
       <h1 className={`text-[#2B2B33] text-3xl lg:text-4xl xl:text-5xl 3xl:text-6xl my-8 tracking-wide mt-0 ml-[10%] pt-3`}>Projects</h1>
       <div className='w-full flex flex-col justify-center items-center lg:items-baseline'>
         <Project 
           staticIMG = {Crush} 
           name = 'Fruit Crush'
-          description= {'Fruit Crush is a puzzle game that is designed to test your skills in matching and crushing fruits of different colors and shapes. The objective of the game is to score as many points as possible within 30 moves. The game features a leaderboard of 10, where the top 10 players with the highest scores will be listed.'}
-          stack = {['Next.js', 'Firebase']}
+          description= {'Fruit Crush is a puzzle game where the players try to score as many points as possible within 30 moves. The game features a leaderboard of 10, where the top 10 players with the highest scores will be listed.'}
+          stack = {[SiNextdotjs, SiFirebase]}
+          live = ''
+          isLive = {true}
+          source= {'https://www.google.com'}
           />
         <Project 
           staticIMG= {Crush} 
           name = 'Personal Web Site'
           description='Personal Web Site' 
-          stack = {['Next.js', 'Tailwind CSS', 'Typescript']}
+          stack = {[SiNextdotjs, SiTailwindcss, SiTypescript]}
+          live = ''
+          isLive = {false}
+          source= {'https://www.google.com'}
           />
       </div>
     </div>
