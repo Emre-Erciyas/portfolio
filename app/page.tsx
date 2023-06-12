aa7087ae382ff82e573fcf8546af04e618c7262d
Emre <emre140498@hotmail.com>
2023-06-12 21:41:19 +0300

    trying ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { useRef } from 'react';
 import dynamic from 'next/dynamic';
-const Header = dynamic(() => import('@/components/header'));
-const About = dynamic(() => import('@/components/about'));
+const Header = dynamic(() => import('@/components/header'), { ssr: false });
+const About = dynamic(() => import('@/components/about'), { ssr: false });
 const Footer = dynamic(() => import('@/components/footer'), { ssr: false });
-const Projects = dynamic(() => import('@/components/projects'));
+const Projects = dynamic(() => import('@/components/projects'), { ssr: false });
 const Main = dynamic(() => import('@/components/main'));
 export default function Home() {
 	const home = useRef(null);
