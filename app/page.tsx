e6c215b3d718919cdf18b99ef9a37647553a331b
Emre <emre140498@hotmail.com>
2023-11-08 19:07:57 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { useRef } from 'react';
 import dynamic from 'next/dynamic';
-const Header = dynamic(() => import('@/components/header'), { ssr: false });
-const About = dynamic(() => import('@/components/about'), { ssr: false });
-const Footer = dynamic(() => import('@/components/footer'), { ssr: false });
-const Projects = dynamic(() => import('@/components/projects'), { ssr: false });
+const Header = dynamic(() => import('@/components/header'));
+const About = dynamic(() => import('@/components/about'));
+const Footer = dynamic(() => import('@/components/footer'));
+const Projects = dynamic(() => import('@/components/projects'));
 const Main = dynamic(() => import('@/components/main'));
 export default function Home() {
 	const home = useRef(null);
