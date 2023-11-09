09a860f2856d3eb0ef5c5d7cb49ea18632ad6549
Emre <emre140498@hotmail.com>
2023-11-09 08:39:51 +0300

    update cv

--- components/footer.tsx
+++ components/footer.tsx
@@ -176,7 +176,7 @@ export default function Footer(props: Props) {
 					</Link>
 					<Link
 						href={
-							'https://drive.google.com/file/d/1NVNXRMAQa7zwxC5YCJp1dkj1bmh5cXyZ/view?usp=sharing'
+							'https://drive.google.com/file/d/1nBsQvcqsw5ua6RNIvgBDP8KUaUPBCGrZ/view?usp=sharing'
 						}
 					>
 						<AiFillFilePdf className="text-[#fefeff] hover:text-[#B1B1BD] duration-[400ms] h-8 w-8 sm:h-10 sm:w-10 md:h-12 md:w-12 lg:w-14 lg:h-14 3xl:w-24 3xl:h-24" />
